bee8ffeaef7bba0cfb9b411e8f320d63473d5e6e
Geison <geisonmalta@hotmail.com>
2022-05-26 01:39:47 -0300

    Condição para as bordas ficarem verdes quando os quadros estiverem na ordem correta de 1,2,3

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let areas = {
+   a: null,
+   b: null,
+   c: null
+};
+
 document.querySelectorAll('.item').forEach(item => {
    item.addEventListener('dragstart', dragStart);
    item.addEventListener('dragend', dragEnd);
@@ -8,6 +14,10 @@ document.querySelectorAll('.area').forEach(area => {
    area.addEventListener('dragleave', dragLeave);
    area.addEventListener('drop', drop);
 });
+
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
 
                      // Funções Item\\
 
@@ -20,11 +30,58 @@ function dragEnd(e){
                      // Funções Area\\
 
 function dragOver(e){
+   if(e.currentTarget.querySelector('.item') === null){
+      e.preventDefault();
+      e.currentTarget.classList.add('hover');
+   }
+  
 
 }
 function dragLeave(e){
-
+   e.currentTarget.classList.remove('hover');
 }
 function drop(e){
+   e.currentTarget.classList.remove('hover');
 
+   if(e.currentTarget.querySelector('.item') === null){
+      let dragItem = document.querySelector('.item.dragging');
+      e.currentTarget.appendChild(dragItem);
+      updateAreas();
+   }
+  
+}
+
+                              //Funções Área Neutra\\
+
+function dragOverNeutral(e){
+   e.preventDefault();
+   e.currentTarget.classList.add('hover');
+}
+function dragLeaveNeutral(E){
+   e.currentTarget.classList.remove('hover');
+}
+function dropNeutral(e){
+   e.currentTarget.classList.remove('hover');
+   let dragItem = document.querySelector('.item.dragging');
+   e.currentTarget.appendChild(dragItem);
+   updateAreas();
+}
+
+                                 //Funções de Lógica do Processo\\
+function updateAreas(){
+   document.querySelectorAll('.area').forEach(area => {
+         let name = area.getAttribute('data-name');
+
+         if(area.querySelector('.item') !== null) {
+            areas[name] = area.querySelector('.item').innerHTML;
+         }else {
+            areas [name] = null;
+         }
+   });
+
+   if(areas.a === '1' && areas.b === '2' && areas.c ==='3'){
+      document.querySelector('.areas').classList.add('correct');
+   }else {
+      document.querySelector('.areas').classList.remove('correct');
+   }
 }
